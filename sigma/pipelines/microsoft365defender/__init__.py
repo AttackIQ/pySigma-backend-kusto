2bcdd71f2bfecb2579f3dc45ac016c1646afc12c
Stephen Lincoln <stephen.lincoln@attackiq.com>
2024-09-08 07:13:56 -0400

    Moved sentinelasim to new folder

--- sigma/pipelines/microsoft365defender/__init__.py
+++ sigma/pipelines/microsoft365defender/__init__.py
@@ -1,7 +1,4 @@
 from .microsoft365defender import microsoft_365_defender_pipeline
-from .sentinelasim import sentinelasim_pipeline
-
 pipelines = {
     "microsoft_365_defender_pipeline": microsoft_365_defender_pipeline,   # TODO: adapt identifier to something approproiate
-    "sentinelasim_pipeline": sentinelasim_pipeline,
 }
---
+++ sigma/pipelines/sentinelasim/__init__.py
@@ -0,0 +1,4 @@
+from .sentinelasim import sentinel_asim_pipeline
+pipelines = {
+    "sentinel_asim": sentinel_asim_pipeline,   # TODO: adapt identifier to something approproiate
+}
