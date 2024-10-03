9bf8b40ae42a31023aadc76bd65c5aec2cb94efa
Stephen Lincoln <stephen.lincoln@attackiq.com>
2024-10-03 14:33:26 -0400

    Formatting

--- sigma/backends/kusto/kusto.py
+++ sigma/backends/kusto/kusto.py
@@ -1,18 +1,21 @@
 import re
-from typing import ClassVar, Dict, Tuple, Pattern, Any, Union, Optional
+from typing import ClassVar, Dict, Pattern, Tuple, Union
 
-from sigma.conditions import ConditionItem, ConditionAND, ConditionOR, ConditionNOT, ConditionFieldEqualsValueExpression
+from sigma.conditions import (
+    ConditionAND,
+    ConditionFieldEqualsValueExpression,
+    ConditionItem,
+    ConditionNOT,
+    ConditionOR,
+)
 from sigma.conversion.base import TextQueryBackend
 from sigma.conversion.deferred import DeferredQueryExpression
 from sigma.conversion.state import ConversionState
-from sigma.processing.pipeline import ProcessingPipeline
-from sigma.rule import SigmaRule
 from sigma.types import SigmaCompareExpression, SigmaString, SpecialChars
 
 
-
 class KustoBackend(TextQueryBackend):
-    """Microsoft 365 Defender KQL Backend. """
+    """Microsoft 365 Defender KQL Backend."""
 
     # The backend generates grouping if required
     name: ClassVar[str] = "Kusto backend"
@@ -26,8 +29,9 @@ class KustoBackend(TextQueryBackend):
     # Operator precedence
     parenthesize = True
     precedence: ClassVar[Tuple[ConditionItem, ConditionItem, ConditionItem]] = (ConditionNOT, ConditionAND, ConditionOR)
-    group_expression: ClassVar[
-        str] = "({expr})"  # Expression for precedence override grouping as format string with {expr} placeholder
+    group_expression: ClassVar[str] = (
+        "({expr})"  # Expression for precedence override grouping as format string with {expr} placeholder
+    )
     # Generated query tokens
     token_separator: str = " "  # separator inserted between all boolean operators
     or_token: ClassVar[str] = "or"
@@ -38,22 +42,26 @@ class KustoBackend(TextQueryBackend):
     # String output
     ## Fields
     ### Quoting
-    field_quote: ClassVar[
-        str] = "'"  # Character used to quote field characters if field_quote_pattern matches (or not, depending on field_quote_pattern_negation). No field name quoting is done if not set.
+    field_quote: ClassVar[str] = (
+        "'"  # Character used to quote field characters if field_quote_pattern matches (or not, depending on field_quote_pattern_negation). No field name quoting is done if not set.
+    )
     field_quote_pattern: ClassVar[Pattern] = re.compile(
-        "^\\w+$")  # Quote field names if this pattern (doesn't) matches, depending on field_quote_pattern_negation. Field name is always quoted if pattern is not set.
-    field_quote_pattern_negation: ClassVar[
-        bool] = True  # Negate field_quote_pattern result. Field name is quoted if pattern doesn't matches if set to True (default).
+        "^\\w+$"
+    )  # Quote field names if this pattern (doesn't) matches, depending on field_quote_pattern_negation. Field name is always quoted if pattern is not set.
+    field_quote_pattern_negation: ClassVar[bool] = (
+        True  # Negate field_quote_pattern result. Field name is quoted if pattern doesn't matches if set to True (default).
+    )
 
     ### Escaping
     field_escape: ClassVar[str] = ""  # Character to escape particular parts defined in field_escape_pattern.
     field_escape_quote: ClassVar[bool] = True  # Escape quote string defined in field_quote
     field_escape_pattern: ClassVar[Pattern] = re.compile(
-        "\\s")  # All matches of this pattern are prepended with the string contained in field_escape.
+        "\\s"
+    )  # All matches of this pattern are prepended with the string contained in field_escape.
 
     ## Values
     str_quote: ClassVar[str] = '"'  # string quoting character (added as escaping character)
-    escape_char: ClassVar[str] = "\\"  # Escaping character for special characrers inside string
+    escape_char: ClassVar[str] = "\\"  # Escaping character for special characters inside string
     wildcard_multi: ClassVar[str] = "*"  # Character used as multi-character wildcard
     wildcard_single: ClassVar[str] = "*"  # Character used as single-character wildcard
     add_escaped: ClassVar[str] = "\\"  # Characters quoted in addition to wildcards and string quote
@@ -67,25 +75,31 @@ class KustoBackend(TextQueryBackend):
     startswith_expression: ClassVar[str] = "{field} startswith {value}"
     endswith_expression: ClassVar[str] = "{field} endswith {value}"
     contains_expression: ClassVar[str] = "{field} contains {value}"
-    wildcard_match_expression: ClassVar[str] = None  # Special expression if wildcards can't be matched with the eq_token operator
+    wildcard_match_expression: ClassVar[str] = (
+        None  # Special expression if wildcards can't be matched with the eq_token operator
+    )
 
     # Regular expressions
-    re_expression: ClassVar[
-        str] = "{field} matches regex \"{regex}\""  # Regular expression query as format string with placeholders {field} and {regex}
+    re_expression: ClassVar[str] = (
+        '{field} matches regex "{regex}"'  # Regular expression query as format string with placeholders {field} and {regex}
+    )
     re_escape_char: ClassVar[str] = "\\"  # Character used for escaping in regular expressions
     re_escape: ClassVar[Tuple[str]] = ()  # List of strings that are escaped
     re_escape_escape_char: bool = True  # If True, the escape character is also escaped
 
     # cidr expressions
     cidr_wildcard: ClassVar[str] = "*"  # Character used as single wildcard
-    cidr_expression: ClassVar[
-        str] = "ipv4_is_in_range({field}, \"{value}\")"  # CIDR expression query as format string with placeholders {field} = {value}
-    cidr_in_list_expression: ClassVar[
-        str] = "ipv4_is_in_any_range({field}, \"{value}\")"  # CIDR expression query as format string with placeholders {field} = in({list})
+    cidr_expression: ClassVar[str] = (
+        'ipv4_is_in_range({field}, "{value}")'  # CIDR expression query as format string with placeholders {field} = {value}
+    )
+    cidr_in_list_expression: ClassVar[str] = (
+        'ipv4_is_in_any_range({field}, "{value}")'  # CIDR expression query as format string with placeholders {field} = in({list})
+    )
 
     # Numeric comparison operators
-    compare_op_expression: ClassVar[
-        str] = "{field} {operator} {value}"  # Compare operation query as format string with placeholders {field}, {operator} and {value}
+    compare_op_expression: ClassVar[str] = (
+        "{field} {operator} {value}"  # Compare operation query as format string with placeholders {field}, {operator} and {value}
+    )
     # Mapping between CompareOperators elements and strings used as replacement for {operator} in compare_op_expression
     compare_operators: ClassVar[Dict[SigmaCompareExpression.CompareOperators, str]] = {
         SigmaCompareExpression.CompareOperators.LT: "<",
@@ -95,29 +109,37 @@ class KustoBackend(TextQueryBackend):
     }
 
     # Null/None expressions
-    field_null_expression: ClassVar[
-        str] = "isnull({field})"  # Expression for field has null value as format string with {field} placeholder for field name
+    field_null_expression: ClassVar[str] = (
+        "isnull({field})"  # Expression for field has null value as format string with {field} placeholder for field name
+    )
 
     # Field value in list, e.g. "field in (value list)" or "field containsall (value list)"
     convert_or_as_in: ClassVar[bool] = True  # Convert OR as in-expression
     convert_and_as_in: ClassVar[bool] = True  # Convert AND as in-expression
-    in_expressions_allow_wildcards: ClassVar[
-        bool] = True  # Values in list can contain wildcards. If set to False (default) only plain values are converted into in-expressions.
-    field_in_list_expression: ClassVar[
-        str] = "{field} {op} ({list})"  # Expression for field in list of values as format string with placeholders {field}, {op} and {list}
-    or_in_operator: ClassVar[
-        str] = "in~"  # Operator used to convert OR into in-expressions. Must be set if convert_or_as_in is set
-    and_in_operator: ClassVar[
-        str] = "has_all"  # Operator used to convert AND into in-expressions. Must be set if convert_and_as_in is set
+    in_expressions_allow_wildcards: ClassVar[bool] = (
+        True  # Values in list can contain wildcards. If set to False (default) only plain values are converted into in-expressions.
+    )
+    field_in_list_expression: ClassVar[str] = (
+        "{field} {op} ({list})"  # Expression for field in list of values as format string with placeholders {field}, {op} and {list}
+    )
+    or_in_operator: ClassVar[str] = (
+        "in~"  # Operator used to convert OR into in-expressions. Must be set if convert_or_as_in is set
+    )
+    and_in_operator: ClassVar[str] = (
+        "has_all"  # Operator used to convert AND into in-expressions. Must be set if convert_and_as_in is set
+    )
     list_separator: ClassVar[str] = ", "  # List element separator
 
     # Value not bound to a field
-    unbound_value_str_expression: ClassVar[
-        str] = '{value}'  # Expression for string value not bound to a field as format string with placeholder {value}
-    unbound_value_num_expression: ClassVar[
-        str] = '{value}'  # Expression for number value not bound to a field as format string with placeholder {value}
-    unbound_value_re_expression: ClassVar[
-        str] = '_=~{value}'  # Expression for regular expression not bound to a field as format string with placeholder {value}
+    unbound_value_str_expression: ClassVar[str] = (
+        "{value}"  # Expression for string value not bound to a field as format string with placeholder {value}
+    )
+    unbound_value_num_expression: ClassVar[str] = (
+        "{value}"  # Expression for number value not bound to a field as format string with placeholder {value}
+    )
+    unbound_value_re_expression: ClassVar[str] = (
+        "_=~{value}"  # Expression for regular expression not bound to a field as format string with placeholder {value}
+    )
 
     # Query finalization: appending and concatenating deferred query part
     deferred_start: ClassVar[str] = "\n| "  # String used as separator between main query and deferred parts
@@ -132,17 +154,18 @@ class KustoBackend(TextQueryBackend):
     # Override methods
 
     #  For numeric values, need == instead of =~
-    def convert_condition_field_eq_val_num(self, cond: ConditionFieldEqualsValueExpression, state: ConversionState) -> \
-            Union[str, DeferredQueryExpression]:
+    def convert_condition_field_eq_val_num(
+        self, cond: ConditionFieldEqualsValueExpression, state: ConversionState
+    ) -> Union[str, DeferredQueryExpression]:
         """Conversion of field = number value expressions"""
         try:
             return self.escape_and_quote_field(cond.field) + self.num_eq_token + str(cond.value)
         except TypeError:  # pragma: no cover
             raise NotImplementedError("Field equals numeric value expressions are not supported by the backend.")
 
-    def convert_condition_as_in_expression(self,
-                                           cond: Union[ConditionOR, ConditionAND],
-                                           state: ConversionState) -> Union[str, DeferredQueryExpression]:
+    def convert_condition_as_in_expression(
+        self, cond: Union[ConditionOR, ConditionAND], state: ConversionState
+    ) -> Union[str, DeferredQueryExpression]:
         """Overridden method for conversion of field in value list conditions.
         KQL doesn't really use wildcards, so if we have an 'as_in' condition where one or more of the values has a wildcard,
         we can still use the as_in condition, then append on the wildcard value(s) with a startswith, endswith, or contains
@@ -152,41 +175,41 @@ class KustoBackend(TextQueryBackend):
         field = self.escape_and_quote_field(cond.args[0].field)
         op1 = self.or_in_operator if isinstance(cond, ConditionOR) else self.and_in_operator
         op2 = self.or_token if isinstance(cond, ConditionOR) else self.and_token
-        list_nonwildcard = self.list_separator.join([
-            self.convert_value_str(arg.value, state)
-            for arg in cond.args
-            if (isinstance(arg.value, SigmaString) and not arg.value.contains_special()) or not isinstance(arg.value,
-                                                                                                           SigmaString)
-        ])
+        list_nonwildcard = self.list_separator.join(
+            [
+                self.convert_value_str(arg.value, state)
+                for arg in cond.args
+                if (isinstance(arg.value, SigmaString) and not arg.value.contains_special())
+                or not isinstance(arg.value, SigmaString)
+            ]
+        )
         list_wildcards = [
             arg.value for arg in cond.args if isinstance(arg.value, SigmaString) and arg.value.contains_special()
         ]
         as_in_expr = ""
         # Convert as_in and wildcard values separately
         if list_nonwildcard:
-            as_in_expr = self.field_in_list_expression.format(
-                field=field,
-                op=op1,
-                list=list_nonwildcard
-            )
+            as_in_expr = self.field_in_list_expression.format(field=field, op=op1, list=list_nonwildcard)
         wildcard_exprs_list = []
         if list_wildcards:
             for arg in list_wildcards:
                 new_cond = ConditionFieldEqualsValueExpression(field=field, value=arg)
                 if arg[1:-1].contains_special():  # Wildcard in string, not at start or end.
                     # We need to get rid of all wildcards, and create a 'and contains' for each element in the list
-                    expr = f'{self.token_separator}{self.and_token}{self.token_separator}'.join(
-                        [self.contains_expression.format(
-                            field=field,
-                            value=self.convert_value_str(
-                                SigmaString(x), state)) for x in arg.s if not isinstance(x, SpecialChars)
+                    expr = f"{self.token_separator}{self.and_token}{self.token_separator}".join(
+                        [
+                            self.contains_expression.format(
+                                field=field, value=self.convert_value_str(SigmaString(x), state)
+                            )
+                            for x in arg.s
+                            if not isinstance(x, SpecialChars)
                         ]
                     )
                     expr = self.group_expression.format(expr=expr)
                 else:
                     expr = self.convert_condition_field_eq_val_str(new_cond, state)
                 wildcard_exprs_list.append(expr)
-        wildcard_exprs = f'{self.token_separator}{op2}{self.token_separator}'.join(wildcard_exprs_list)
+        wildcard_exprs = f"{self.token_separator}{op2}{self.token_separator}".join(wildcard_exprs_list)
         if as_in_expr and wildcard_exprs:
             return as_in_expr + self.token_separator + op2 + self.token_separator + wildcard_exprs
         return as_in_expr + wildcard_exprs
