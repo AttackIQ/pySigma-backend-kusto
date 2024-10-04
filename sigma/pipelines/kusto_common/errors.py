0843c59a2ccbf3edf0ac8351baf1688beb374af6
Stephen Lincoln <stephen.lincoln@attackiq.com>
2024-10-04 10:49:24 -0400

    Fixed invalid field error raising on keywords (no field names)

--- sigma/pipelines/kusto_common/errors.py
+++ sigma/pipelines/kusto_common/errors.py
@@ -13,8 +13,9 @@ class InvalidFieldTransformation(DetectionItemFailureTransformation):
 
     def apply_detection_item(self, detection_item: SigmaDetectionItem) -> None:
         field_name = detection_item.field
-        self.message = f"Invalid SigmaDetectionItem field name encountered: {field_name}. " + self.message
-        raise SigmaTransformationError(self.message)
+        if field_name:  # If no field name is set, don't raise an error because its a keyword
+            self.message = f"Invalid SigmaDetectionItem field name encountered: {field_name}. " + self.message
+            raise SigmaTransformationError(self.message)
 
 
 class InvalidHashAlgorithmError(Exception):
